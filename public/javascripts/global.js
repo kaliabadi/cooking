1bcc4800f3fa3ffb039063cd312dc6648bf68313
Kouros Aliabadi <kaliabadi@Kouross-Air.home>
2016-09-21 21:21:53 +0100

    some styling and new background image

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,34 +3,30 @@ var documentListData = [];
 // DOM Ready =============================================================
 $(document).ready(function() {
 
-    // Populate the user table on initial page load
     populateTable();
 
-     // Recipe Name link click
     $('#documentList table tbody').on('click', 'td a.linkshowrecipedetails', showRecipeDetails);
-
-    // Add User button click
+    
     $('#btnAddRecipe').on('click', addRecipe);
 
-    // Delete User link click
     $('#documentList table tbody').on('click', 'td a.linkdeleterecipe', deleteRecipe);
+
+    $('#btnSearch').on('click', addShownClass);
 
     $('#btnSearch').on('click', searchHttp);
 });
 
-// Functions =============================================================
+function addShownClass() {
+    $('.searchList').addClass('shown');
+};
 
-// Fill table with data
 function populateTable() {
 
-    // Empty content string
     var tableContent = '';
 
-    // jQuery AJAX call for JSON
     $.getJSON( '/documents/documentlist', function( data ) {
         documentListData = data;
 
-        // For each item in our JSON, add a table row and cells to the content string
         $.each(data, function(){
             tableContent += '<tr>';
             tableContent += '<td><a href="#" class="linkshowrecipedetails" rel="' + this.recipeName + '">' + this.recipeName + '</a></td>';
@@ -39,29 +35,22 @@ function populateTable() {
             tableContent += '</tr>';
         });
 
-        // Inject the whole content string into our existing HTML table
         $('#documentList table tbody').html(tableContent);
     });
 };
 
-/// Show User Info
 function showRecipeDetails(event) {
 
-    // Prevent Link from Firing
     event.preventDefault();
 
-    // Retrieve username from link rel attribute
     var thisRecipeName = $(this).attr('rel');
 
-    // Get Index of object based on id value
     var arrayPosition = documentListData.map(function(arrayItem) { 
         return arrayItem.recipeName;
     }).indexOf(thisRecipeName);
 
-    // Get our User Object
     var thisRecipeObject = documentListData[arrayPosition];
 
-    //Populate Info Box
     $('#recipeInfoName').text(thisRecipeObject.recipeName);
     $('#recipeInfoCookingTime').text(thisRecipeObject.cookingTime);
     $('#recipeInfoIngredients').text(thisRecipeObject.ingredients);
@@ -82,8 +71,7 @@ function searchHttp(event) {
             tableContent += '<td><a href="' + this.recipeName + '" class="linkshowrecipedetails" rel="something">' + this.recipeName + '</a></td>';
             tableContent += '</tr>';
         });
-
-        $('#searchList table tbody').html(tableContent);
+        $('.searchList table tbody').html(tableContent);
     });
     
     $.getJSON( '/documents/recipesearch/' + query, function( data ) {
@@ -92,24 +80,20 @@ function searchHttp(event) {
             tableContent += '<td><a href="' + this.source_url + '" class="linkshowrecipedetailss" rel="' + this.title + '">' + this.title + '</a><img src="' + this.image_url + '"></img></td>';
             tableContent += '</tr>';
         });
-        $('#searchList table tbody').html(tableContent);
+        $('.searchList table tbody').html(tableContent);
     });
 };
 
-// Add User
 function addRecipe(event) {
     event.preventDefault();
 
-    // Super basic validation - increase errorCount variable if any fields are blank
     var errorCount = 0;
     $('#addRecipe input').each(function(index, val) {
         if($(this).val() === '') { errorCount++; }
     });
 
-    // Check and make sure errorCount's still at zero
     if(errorCount === 0) {
 
-        // If it is, compile all user info into one object
         var newUser = {
             'recipeName': $('#addRecipe fieldset input#inputRecipeName').val(),
             'cookingTime': $('#addRecipe fieldset input#inputCookingTime').val(),
@@ -124,26 +108,16 @@ function addRecipe(event) {
             dataType: 'JSON'
         }).done(function( response ) {
 
-            // Check for successful (blank) response
             if (response.msg === '') {
-
-                // Clear the form inputs
                 $('#addRecipe fieldset input').val('');
-
-                // Update the table
                 populateTable();
-
             }
             else {
-
-                // If something goes wrong, alert the error message that our service returned
-                alert('Error: ' + response.msg);
-
+               alert('Error: ' + response.msg);
             }
         });
     }
     else {
-        // If errorCount is more than 0, error out
         alert('Please fill in all fields');
         return false;
     }
@@ -152,26 +126,21 @@ function addRecipe(event) {
 function deleteRecipe(event) {
     event.preventDefault();
 
-    // Pop up a confirmation dialog
     var confirmation = confirm('Are you sure you want to delete this user?');
 
-    // Check and make sure the user confirmed
     if (confirmation === true) {
 
-        // If they did, do our delete
         $.ajax({
             type: 'DELETE',
             url: '/documents/deletedocument/' + $(this).attr('rel')
         }).done(function( response ) {
 
-            // Check for a successful (blank) response
             if (response.msg === '') {
             }
             else {
                 alert('Error: ' + response.msg);
             }
 
-            // Update the table
             populateTable();
 
         });
---
+++ routes/README.md
@@ -0,0 +1,12 @@
+start mondo db with
+
+mongod -dbpath then a /data root in your repo
+
+then in the location of mongo instilation, generally /usr/local/bin run 
+mongo
+
+then npm i
+
+then npm start
+
+and go to localhost:3000
