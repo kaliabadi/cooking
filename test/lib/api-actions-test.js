71d88926c850fa8b8c00d536511bc4b2fb5b462f
Kouros Aliabadi <kouros_alilabadi@hotmail.com>
2017-06-18 20:26:35 +0100

    adding remove document functionality

--- test/lib/api-actions-test.js
+++ test/lib/api-actions-test.js
@@ -6,6 +6,10 @@ var assert = chai.assert;
 
 var recipeCollection = db.collection(config.recipes);
 
+var testDocument = {
+    'title': 'test title'
+};
+
 describe('api actions', function() {
 
     before(function() {
@@ -22,9 +26,6 @@ describe('api actions', function() {
 
     describe('add a document', function() {
         it('documents added to the recipe collection should be retrievable', function(done) {
-            var testDocument = {
-                'title': 'test title'
-            };
 
             apiActions.addDocument(recipeCollection, testDocument);
 
@@ -38,6 +39,54 @@ describe('api actions', function() {
                 }
             })
         })
-        
+    });
+
+    describe('get documents by title', function() {
+        it('get document details from title', function(done) {
+            apiActions.addDocument(recipeCollection, testDocument);
+
+            recipeCollection.find({}, {}, function(e, docs) {
+                try {
+                    assert.equal(docs.length, 1);
+                } catch (e) {
+                    done(e);
+                }
+            });
+
+            apiActions.getDocumentsByTitle(recipeCollection, testDocument.title, function (docs) {
+                try{
+                    assert.equal(docs[0].title, testDocument.title);
+                    done();
+                } catch(e) {
+                    done(e);
+                }
+            })
+        })
+    });
+    
+    describe('remove a document', function() {
+        it('documents should be removable', function(done) {
+
+            apiActions.addDocument(recipeCollection, testDocument);
+            
+            apiActions.getDocumentsByTitle(recipeCollection, testDocument.title, function (docs) {
+                try{
+                    assert.equal(docs.length, 1);
+                }catch (e) {
+                    done(e);
+                }
+
+                apiActions.removeDocument(recipeCollection, docs[0]._id, function () {
+                    recipeCollection.find({}, {}, function(e, docs) {
+                        try {
+                            assert.equal(docs.length, 0);
+                            done();
+                        } catch (e) {
+                            done(e);
+                        }
+                    })
+                });
+            });
+        })    
     })
 });
