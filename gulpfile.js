065726831c1aad90b635c6b490b80fe9f04d6504
Miles Illsley <milesillsley@ECS0615.local>
2017-11-09 15:04:11 +0000

    gulp now shuts the server down after tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,37 @@
 var gulp = require('gulp');
-var exec = require('child_process').exec
+var exec = require('child_process').exec;
+var gls = require('gulp-live-server');
 const Launcher = require('webdriverio/build/lib/launcher');
 const path = require('path');
 const wdio = new Launcher('wdio.conf.js');
+
+let server;
 
 gulp.task('mongodb', function() {
   exec('mongod --dbpath ./data', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
-    cb(err);
   });
 });
 
-gulp.task('server', ['mongodb'], function() {
-  exec('node ./bin/www', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
+gulp.task('http', ['mongodb'], function(done) {
+    server = gls.new('./bin/www');
+    server.start();
+    return wdio.run(code => {
+      process.exit(code);
+    }, error => {
+      console.error('Launcher failed to run the test', error.stacktrace);
+      process.exit(1);
+    });
 });
 
-gulp.task('e2e', ['server'], function() {
-  return wdio.run(code => {
-    process.exit(code);
-  }, error => {
-    console.error('Launcher failed to run the test', error.stacktrace);
-    process.exit(1);
-  });
-});
-
-gulp.task('endserver', ['e2e'], () => {
-  exec('killall node', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-});
-
-gulp.task('endmongo', ['endserver'], () => {
+gulp.task('stopmongo', ['http'], () => {
   exec('mongo admin --eval "db.shutdownServer()";', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
-    cb(err);
   });
 });
 
-gulp.task('test', ['endmongo'], () => {
-
-})
+gulp.task('test', ['stopmongo'], () => {
+  server.stop();
+});
