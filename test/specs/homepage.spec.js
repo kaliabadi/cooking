5884ade10a889143a445036b83874289f2d560ce
Tom Brandon <Tom@ECS0484.local>
2018-03-23 14:58:17 +0000

    UI test for navigating to the register page from the home page

--- test/specs/homepage.spec.js
+++ test/specs/homepage.spec.js
@@ -2,21 +2,28 @@ import HomePage from '../page-objects/home.page';
 
 describe('Cooking homepage', () => {
 
-  before ('Navigate to Homepage', () => {
-    browser.url('/');
+    before('Navigate to Homepage', () => {
+        browser.url('/');
     });
 
-  it('should navigate to contact page', () => {
-    HomePage.contact.moveToObject();
-    HomePage.contact.waitForVisible();
-    HomePage.contact.click();
-    expect(browser.getUrl()).to.contain('/contact');
-  });
+    it('should navigate to contact page', () => {
+        HomePage.contact.moveToObject();
+        HomePage.contact.waitForVisible();
+        HomePage.contact.click();
+        expect(browser.getUrl()).to.contain('/contact');
+    });
 
-  it('should navigate to recipe store page', () => {
-    HomePage.recipeStore.moveToObject();
-    HomePage.recipeStore.waitForVisible();
-    HomePage.recipeStore.click();
-    expect(browser.getUrl()).to.contain('/recipeStore');
-  });
+    it('should navigate to recipe store page', () => {
+        HomePage.recipeStore.moveToObject();
+        HomePage.recipeStore.waitForVisible();
+        HomePage.recipeStore.click();
+        expect(browser.getUrl()).to.contain('/recipeStore');
+    });
+
+    it('should navigate to the register page', () => {
+        HomePage.register.moveToObject();
+        HomePage.register.waitForVisible();
+        HomePage.register.click();
+        expect(browser.getUrl()).to.contain('/register');
+    });
 });
