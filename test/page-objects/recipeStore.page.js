c7f44b9486310888ce68f79e9c1b0655ecc59db5
Charles Emery <Charles@ECS0617.local>
2018-03-27 11:25:59 +0100

    Finished RecipeStore testing

--- test/page-objects/recipeStore.page.js
+++ test/page-objects/recipeStore.page.js
@@ -1,6 +1,9 @@
 class RecipeStore {
 
     get search() { return $('a[href="/"]'); }
+    get contact() { return $('a[href="/contact"]'); }
+    get login() { return $('a[href="/login"]'); }
+    get register() { return $('a[href="/register"]'); }
 
 }
 export default new RecipeStore();
--- test/specs/recipestore.spec.js
+++ test/specs/recipestore.spec.js
@@ -12,4 +12,25 @@ describe('Cooking homepage', () => {
         RecipeStore.search.click();
         expect(browser.getUrl()).to.equal('http://localhost:3000/');
     });
+
+    it('should navigate to contact page', () => {
+        RecipeStore.contact.moveToObject();
+        RecipeStore.contact.waitForVisible();
+        RecipeStore.contact.click();
+        expect(browser.getUrl()).to.contain('/contact');
+    });
+
+    it('should navigate to login page', () => {
+        RecipeStore.login.moveToObject();
+        RecipeStore.login.waitForVisible();
+        RecipeStore.login.click();
+        expect(browser.getUrl()).to.contain('/login');
+    });
+
+    it('should navigate to the register page', () => {
+        RecipeStore.register.moveToObject();
+        RecipeStore.register.waitForVisible();
+        RecipeStore.register.click();
+        expect(browser.getUrl()).to.contain('/register');
+    });
 });
